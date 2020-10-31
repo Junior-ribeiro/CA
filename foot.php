569d565308d70310cd96ee40fc6ff9a3f90f709b
Your Name <you@example.com>
2020-10-31 16:00:48 +0000

    first commit

---
+++ foot.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+   <div class="container">
+
+        <div class="box">
+
+
+
+    
+  <p class="footer"><a href="mailto:juniorvzpaz@hotmail.com">juniorvzpaz@hotmail.com</a></p>
+
+
+   <?php
+   echo date('d/m/y '); ?> <br>
+   <?php echo date('h:i')
+   ?>   
+   </div>
+
+</body>
+</html>
+
---
+++ header.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    
+
+
+    <link rel="stylesheet" href="css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home page</title>
+</head>
+<h1 class="h1">MY PHP </h1>
+
+
+
+<body>
+   
+
+    <button class="button-1"><a href="index.php">Home</a></button>
+     <button class="button-1"><a href="page2.php">Table</a></button>
+     <button class="button-1"><a href="page3.php">Form</a></button> 
+    
+    
+</body>
+
+</html>
